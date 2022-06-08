a2a268bfa508ae921b2010f7254a5a3311f82fff
Marttins23 <mateus_martins18@yahoo.com.br>
2022-06-08 15:04:15 -0300

    Estrutura Parcialmente finalizada
    Seções praticamente finalizadas. Pendentes de algumas alterações relacionadas a estilizações e alguns detalhes de responsividade.

---
+++ js/index.js
@@ -0,0 +1,10 @@
+$(function () {
+    $(document).scroll(function () {
+        var $nav = $(".fixed-top");
+        $nav.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
+      });
+  });
+
+  $('.carousel').carousel({
+    pause: "false"
+  })
